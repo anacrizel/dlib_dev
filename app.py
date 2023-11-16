aa536cbffa3f2fc72e6767d8edef7bfec87cb6c8
Kiana Crizel Rojo Tiano <97926656+anacrizel@users.noreply.github.com>
2023-11-16 17:22:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import av
 from imutils import face_utils
 import dlib
 import cv2
-import numpy as np
 
 st.title("Streamlit WebRTC using DLIB")
 st.write("This is a sample to integrate DLIB :D ")
@@ -17,13 +16,13 @@ predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 class VideoProcessor:
 
-# This below mehtod will draw all those points which are from 0 to 67 on face one by one.
+    # This below method will draw all those points which are from 0 to 67 on face one by one.
     def drawPoints(image, faceLandmarks, startpoint, endpoint, isClosed=False):
         points = []
         for i in range(startpoint, endpoint+1):
             point = [faceLandmarks.part(i).x, faceLandmarks.part(i).y]
             points.append(point)
-        
+
         points = np.array(points, dtype=np.int32)
         cv2.polylines(image, [points], isClosed, (255, 200, 0), thickness=2, lineType=cv2.LINE_8)
     def recv(self, frame):
@@ -34,17 +33,17 @@ class VideoProcessor:
         rects = detector(gray, 0)
         
         for (i, rect) in enumerate(rects):
-            s_ = predictor(gray, rect)
-            s = face_utils.shape_to_np(s_)
+        	s_ = predictor(gray, rect)
+        	s = face_utils.shape_to_np(s_)
 
         	#for(i, y) in s:
-        		#cv2.circle(img, (i,y), 2, (0, 255, 0), -1)
+        	#cv2.circle(img, (i,y), 2, (0, 255, 0), -1)
         
             #count number of landmarks we actually detected on image
 
             if i==0:
                 print("Total number of face landmarks detected ",len(s_.parts()))
-                    
+                
                 drawPoints(img, s_, 0, 16)           # Jaw line
                 drawPoints(img, s_, 17, 21)          # Left eyebrow
                 drawPoints(img, s_, 22, 26)          # Right eyebrow
@@ -54,6 +53,8 @@ class VideoProcessor:
                 drawPoints(img, s_, 42, 47, True)    # Right Eye
                 drawPoints(img, s_, 48, 59, True)    # Outer lip
                 drawPoints(img, s_, 60, 67, True)    # Inner lip
+        
+                
 
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
@@ -68,8 +69,3 @@ webrtc_streamer(key="dlib", mode=WebRtcMode.SENDRECV,
                               video_processor_factory=VideoProcessor,
                               media_stream_constraints={"video": True, "audio": False},
                               async_processing=True)
-
-
-
-
-
